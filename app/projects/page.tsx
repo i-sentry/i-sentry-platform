562457aeec52e4c9fff0879db5face3bc483f00e
atimscreative <timilehinsunday144@gmail.com>
2024-10-26 02:49:55 +0100

    Adjustment to theme variable colors

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section className="">
+        <div className="wrapper">
+          <div>
+            <p className="">Case Studies</p>
+            <h2 className="">Crafting unique solutions</h2>
+            <p className="">
+              Ready to transform your ideas into reality? Whether you&apos;re
+              looking for digital solutions or interested in our mentorship
+              program, we&apos;re here to help.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
