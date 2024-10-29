65eb2cd0d85059db8da1765d1cfbb92357126c9c
atimscreative <timilehinsunday144@gmail.com>
2024-10-29 16:13:45 +0100

    Update on navbar

--- components/mobile-sidemenu.tsx
+++ components/mobile-sidemenu.tsx
@@ -16,40 +16,37 @@ const MobileSideMenu = () => {
   return (
     <div
       className={cn(
-        "fixed top-0 z-50 h-full w-full duration-300",
+        "fixed top-0 z-50 h-full w-full duration-300 lg:hidden",
         open ? "right-0" : "-right-full",
       )}
     >
-      <div className="bg-primary-900/50 absolute right-0 top-0 h-full w-full backdrop-blur"></div>
-      <div className="bg-primary-900 absolute right-0 top-0 z-10 h-full w-full overflow-hidden p-5 sm:max-w-sm">
+      <div className="absolute right-0 top-0 h-full w-full bg-primary-900/50 backdrop-blur"></div>
+      <div className="absolute right-0 top-0 z-10 h-full w-full overflow-hidden bg-primary-900 p-5 sm:max-w-sm">
         <Image
           src={Cone}
           alt="svg"
-          className="absolute -right-[5rem] top-0 z-0 opacity-30"
+          className="absolute -right-[5rem] top-0 z-0 scale-125 opacity-50"
         />
         <span
           onClick={setOpen}
-          className="absolute right-5 top-5 cursor-pointer text-white"
+          className="absolute right-5 top-5 z-10 cursor-pointer text-white"
         >
           <X size={24} />
         </span>
 
-        <div className="relative z-10 mt-16 inline-flex flex-col">
+        <div className="relative z-10 mt-16 inline-flex flex-col justify-center gap-8">
           {navMenu.map((link: { name: string; url: string }, index: number) => (
             <Link
               href={link.url}
               key={index}
-              className={cn(
-                "text-secondary-300 p-0.5",
-                pathname?.endsWith(link.url) &&
-                  "text-primary-100 rounded-full bg-gradient-to-l from-[#10213e]/50 to-[#061935]/50",
-              )}
+              onClick={setOpen}
+              className={cn("text-2xl uppercase text-secondary-300")}
             >
               <span
                 className={cn(
-                  "inline-block rounded-full p-3",
+                  "inline-block rounded-full",
                   pathname?.endsWith(link.url) &&
-                    "bg-gradient-to-l from-[#10213e]/30 to-[#061935]/30",
+                    "bg-grad bg-clip-text font-semibold text-transparent drop-shadow-[0_4px_2px_rgba(255,255,255,0.2)]",
                 )}
               >
                 {link.name}
@@ -59,7 +56,7 @@ const MobileSideMenu = () => {
         </div>
 
         {/* CTA BUTTONs */}
-        <div className="mt-5 flex flex-col items-center gap-6 border-t pt-8">
+        <div className="mt-10 flex flex-col items-start gap-6 border-t border-secondary-300/10 pt-8">
           <Link
             href="/login"
             className="hidden px-8 font-dm-sans font-normal text-white drop-shadow-[0_7px_3px_rgba(255,255,255,0.3)] lg:inline-block"
