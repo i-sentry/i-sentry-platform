91c93ed72f2c4cddedbe4bb2b449e5a2fc55956e
atimscreative <timilehinsunday144@gmail.com>
2024-11-08 02:44:26 +0100

    Careers concluded

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,12 +1,23 @@
 import SmartButton from "@/components/custom_button";
-import { Zap } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import {
+  ChevronDown,
+  CircleDollarSign,
+  Clock,
+  MapPin,
+  Zap,
+} from "lucide-react";
+import Image from "next/image";
 import React from "react";
+import ReactCountryFlag from "react-country-flag";
+import CareerImg from "@/public/images/career.jpeg";
+import Stacks from "@/public/images/pictures-stack.png";
 
 const Career = () => {
   return (
     <>
       <section className="pb-20 pt-28 lg:pt-40">
-        <div className="wrapper md:mx-auto md:max-w-4xl xl:max-w-5xl">
+        <div className="wrapper">
           <div className="text-center md:mx-auto md:max-w-xl">
             <h4 className="text-sm text-secondary-200">Careers</h4>
             <h1 className="my-4 text-3xl font-medium leading-[1.3] text-white sm:text-5xl sm:leading-snug">
@@ -20,12 +31,16 @@ const Career = () => {
               className="mt-10"
             />
           </div>
+          <div className="relative">
+            <div className="bg-stack absolute -bottom-2 left-0 h-full w-full"></div>
+            <Image src={Stacks} alt="image mansory" className="w-full" />
+          </div>
         </div>
       </section>
 
       {/* JOIN TEAM */}
       <section className="wrapper">
-        <div className="">
+        <div className="md:w-3/4 lg:w-1/2">
           <h3 className="mb-2 font-dm-sans text-2xl text-white">
             Join a team of makers
           </h3>
@@ -44,7 +59,8 @@ const Career = () => {
           />
         </div>
 
-        <div className="mt-14 grid gap-10 sm:grid-cols-2 lg:grid-cols-3">
+        {/* KPI */}
+        <div className="my-14 grid gap-10 sm:grid-cols-2 xl:grid-cols-3">
           {KPIs.map((kpi, index) => {
             return (
               <div
@@ -68,9 +84,158 @@ const Career = () => {
 
       <section className="bg-footer2 py-10">
         <div className="wrapper">
-          <div>
-            
+          <div className="mb-6 sm:grid sm:grid-cols-[1.5fr_1fr] sm:items-start sm:justify-between sm:gap-6">
+            <div className="">
+              <h3 className="mb-5 font-dm-sans text-xl font-normal text-white sm:text-2xl md:text-3xl">
+                Start doing work that matters
+              </h3>
+              <p className="font-light text-primary-100">
+                Want to work with some of the best global talent and build
+                software used by all the companies you know and love? Join the
+                team — we&apos;re hiring!
+              </p>
+            </div>
+            <div className="mt-8 items-center gap-2.5 sm:mt-0 sm:inline-flex sm:justify-end">
+              <span className="hidden text-sm text-primary-50 sm:inline-block">
+                Location:
+              </span>
+              <div className="flex items-center justify-between rounded-[8px] bg-grad p-3 sm:gap-10 sm:p-1.5">
+                <div className="inline-flex items-center gap-2 text-primary-50 sm:text-xs">
+                  <MapPin size={20} className="sm:w-4" />
+                  <span>Worldwide</span>
+                </div>
+
+                <ChevronDown size={20} className="text-primary-50" />
+              </div>
+            </div>
           </div>
+
+          <div className="space-y-6">
+            {/* DESIGN JOBS */}
+            <div className="pt-10 sm:border-t sm:border-[#FAFAFA26] md:grid md:grid-cols-2 md:gap-6 lg:grid-cols-[1fr_1.4fr]">
+              <div>
+                <h3 className="font-dm-sans text-lg font-medium text-white">
+                  Design
+                </h3>
+                <p className="font-light text-primary-100">
+                  Open positions in our design team.
+                </p>
+              </div>
+
+              {/* JOB INFO */}
+              <div className="mt-6 rounded-2xl border border-[#FAFAFA26] p-5 md:mt-0">
+                <div className="lg:flex lg:items-center lg:gap-2">
+                  <h4 className="mb-2 font-dm-sans text-lg font-medium text-white lg:mb-0">
+                    Product Designer
+                  </h4>
+                  <div className="flex items-center gap-2 lg:grow lg:justify-between">
+                    <Badge className="bg-career inline-flex items-center gap-2 rounded-[6px] px-2 py-0.5 font-inter text-sm font-normal text-primary-50">
+                      <span className="h-1.5 w-1.5 rounded-full bg-[#2E90FA]"></span>{" "}
+                      Design
+                    </Badge>
+                    <Badge className="bg-career inline-flex items-center gap-2 rounded-[6px] px-2 py-0.5 font-inter text-sm font-normal text-primary-50">
+                      <span className="inline-block h-4 w-4 rounded-full text-primary-50">
+                        <ReactCountryFlag
+                          svg
+                          countryCode="AU"
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                          }}
+                          className="rounded-full object-cover"
+                        />
+                      </span>{" "}
+                      Melbourne, Australia
+                    </Badge>
+                  </div>
+                </div>
+                <p className="mb-8 mt-2 font-light text-primary-100">
+                  We&apos;re looking for a mid-level product designer to join
+                  our team.
+                </p>
+                <div className="inline-flex items-center gap-6">
+                  <div className="inline-flex items-center gap-2 text-primary-100">
+                    <span>
+                      <Clock size={20} />
+                    </span>
+                    Full time
+                  </div>
+                  <div className="inline-flex items-center gap-2 text-primary-100">
+                    <span>
+                      <CircleDollarSign size={20} />
+                    </span>
+                    80k - 100k
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* SOFTWARE JOBS */}
+            <div className="border-t border-[#FAFAFA26] pt-10 md:grid md:grid-cols-2 md:gap-6 lg:grid-cols-[1fr_1.4fr]">
+              {/* HEADING */}
+              <div>
+                <h3 className="font-dm-sans text-lg font-medium text-white">
+                  Software Engineering
+                </h3>
+                <p className="font-light text-primary-100">
+                  Open positions in our development team.
+                </p>
+              </div>
+
+              {/* JOB INFO */}
+              <div className="mt-6 rounded-2xl border border-[#FAFAFA26] p-5 md:mt-0">
+                <div className="lg:flex lg:items-center lg:gap-2">
+                  <h4 className="mb-2 font-dm-sans text-lg font-medium text-white lg:mb-0">
+                    Backend Engineer
+                  </h4>
+                  <div className="flex items-center gap-2 lg:grow lg:justify-between">
+                    <Badge className="bg-career inline-flex items-center gap-2 rounded-[6px] px-2 py-0.5 font-inter text-sm font-normal text-primary-50">
+                      <span className="h-1.5 w-1.5 rounded-full bg-[#2E90FA]"></span>{" "}
+                      Design
+                    </Badge>
+                    <Badge className="bg-career inline-flex items-center gap-2 rounded-[6px] px-2 py-0.5 font-inter text-sm font-normal text-primary-50">
+                      <span className="inline-block h-4 w-4 rounded-full text-primary-50">
+                        <ReactCountryFlag
+                          svg
+                          countryCode="AU"
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                          }}
+                          className="rounded-full object-cover"
+                        />
+                      </span>{" "}
+                      Melbourne, Australia
+                    </Badge>
+                  </div>
+                </div>
+                <p className="mb-8 mt-2 font-light text-primary-100">
+                  We&apos;re looking for a mid-level product designer to join
+                  our team.
+                </p>
+                <div className="inline-flex items-center gap-6">
+                  <div className="inline-flex items-center gap-2 text-primary-100">
+                    <span>
+                      <Clock size={20} />
+                    </span>
+                    Full time
+                  </div>
+                  <div className="inline-flex items-center gap-2 text-primary-100">
+                    <span>
+                      <CircleDollarSign size={20} />
+                    </span>
+                    80k - 100k
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Image
+            src={CareerImg}
+            alt="office-room"
+            className="mt-6 h-[480px] w-full rounded-xl object-cover object-[right_center]"
+          />
         </div>
       </section>
     </>
