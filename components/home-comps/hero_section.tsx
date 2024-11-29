56be645661ae6d11c875679fd822f513cec4af8a
atimscreative <timilehinsunday144@gmail.com>
2024-11-29 02:24:55 +0100

    Added animation of hero text

--- components/home-comps/hero_section.tsx
+++ components/home-comps/hero_section.tsx
@@ -1,8 +1,37 @@
 import React from "react";
 import SmartButton from "../custom_button";
 import AnimatedCircle from "../animate_circles";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
+  // useGSAP(() => {
+  //   gsap.from("#hero-title", {
+  //     y: -100,
+  //     duration: 2.5,
+  //     opacity: 0,
+  //     ease: "power2.inOut",
+  //   });
+  // }, []);
+
+  useGSAP(() => {
+    gsap.from(["#hero-title", "#sub-text", ".btn-1", ".btn-2"], {
+      y: 300,
+      opacity: 0,
+      duration: 3,
+      // touchAction: "play reverse play reverse",
+      stagger: {
+        // implemeting the stagger effect
+        amount: 1,
+        from: "start",
+      },
+      ease: "power2.inOut",
+    });
+  }, []);
+
   return (
     <>
       <section className="relative min-h-[30vh] w-full overflow-hidden py-36 lg:flex lg:h-screen lg:items-center lg:justify-center lg:py-20">
@@ -11,10 +40,16 @@ const Hero = () => {
             <AnimatedCircle />
           </>
           <div className="relative z-10 mx-auto flex flex-col items-center md:max-w-2xl lg:max-w-4xl">
-            <h1 className="text-center text-[28px] font-semibold text-white sm:text-3xl md:text-5xl lg:text-6xl">
+            <h1
+              id="hero-title"
+              className="text-center text-[28px] font-semibold text-white sm:text-3xl md:text-5xl lg:text-6xl"
+            >
               Pioneering the Digital Frontier
             </h1>
-            <p className="mb-8 mt-4 text-center text-sm font-extralight leading-6 text-[#C2C2C2CC] sm:mx-auto sm:w-[80%] lg:max-w-xl lg:text-lg">
+            <p
+              id="sub-text"
+              className="mb-8 mt-4 text-center text-sm font-extralight leading-6 text-[#C2C2C2CC] sm:mx-auto sm:w-[80%] lg:max-w-xl lg:text-lg"
+            >
               Our cutting-edge solutions and global expertise empower businesses
               to navigate and thrive in the digital landscape of tomorrow.
             </p>
@@ -25,12 +60,14 @@ const Hero = () => {
                 url="/services"
                 variant="dark"
                 buttonText="Our Services"
+                className="btn-1"
               />
               <SmartButton
                 isLink
                 url="/mentorships"
                 variant="bright"
                 buttonText="Join our Program"
+                className="btn-2"
               />
             </div>
           </div>
