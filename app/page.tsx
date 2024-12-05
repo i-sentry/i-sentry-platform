94e28df52747d92c373f58964ac1f5a64b43f84f
atimscreative <timilehinsunday144@gmail.com>
2024-12-06 00:22:20 +0100

    Fixed error redirection

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,18 @@ import Testimonials from "@/components/home-comps/testimonials";
 import MenteeSection from "@/components/home-comps/mentees_section";
 import Career from "@/components/home-comps/career_section";
 import Partners from "@/components/home-comps/partners_section";
-import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/dist/ScrollSmoother";
-import { useEffect } from "react";
-
-gsap.registerPlugin(ScrollSmoother);
 
 export default function Home() {
-  useEffect(() => {
-    if (typeof window !== "undefined" && !ScrollSmoother.get()) {
-      ScrollSmoother.create({
-        wrapper: "#wrapper",
-        content: "#content",
-        smooth: 1, // Smoothness level
-        effects: true, // Enables effects like parallax
-      });
-    }
-  }, []);
   return (
-    <main id="wrapper">
-      <div id="content">
-        <Hero />
-        <Partners />
-        <About />
-        <Services />
-        <Statistics />
-        <MenteeSection />
-        <Testimonials />
-        <Career />
-      </div>
-    </main>
+    <>
+      <Hero />
+      <Partners />
+      <About />
+      <Services />
+      <Statistics />
+      <MenteeSection />
+      <Testimonials />
+      <Career />
+    </>
   );
 }
