593ab72bff854580504ff820d463864ab0f45920
atimscreative <timilehinsunday144@gmail.com>
2024-12-09 01:26:23 +0100

    Mentorship content in progress

--- app/mentorships/page.tsx
+++ app/mentorships/page.tsx
@@ -4,6 +4,7 @@ import { Zap } from "lucide-react";
 import React from "react";
 import InternImage from "@/public/images/internship.jpeg";
 import Image from "next/image";
+import BannerIllustration from "@/components/BannerIllustration";
 
 export type PriceListProp = {
   package: string;
@@ -11,54 +12,65 @@ export type PriceListProp = {
   description: string;
   features: string[];
   badge: string;
-  
+  comingSoon: boolean;
 };
+
+/* 
+
+
+*/
 
 const priceList: PriceListProp[] = [
   {
     package: "Starter Mentorship",
     duration: "3 Months",
-    description: "Lorem Ipsum",
+    description:
+      "Perfect for beginners and students starting their professional journey.",
     features: [
-      "1-on-1 monthly sessions",
-      "Career guidance",
-      "Resume review",
-      "Basic project feedback",
-      "Email support",
-      "Email support",
-      "Email support",
+      "Weekly 1-on-1 mentorship sessions.",
+      "Career guidance and resume review.",
+      "Access to our WhatsApp Group for peer support and mentor advice.",
+      "Basic project feedback.",
+      "Hands-on project management using Jira for task organization.",
+      "Basic certification upon completion.",
+      "Access to exclusive career resources, including templates, guides, and recorded workshops.",
     ],
     badge: "",
+    comingSoon: false,
   },
   {
     package: "Professional Growth",
     duration: "6 Months",
-    description: "Lorem Ipsum",
+    description:
+      "Ideal for professionals eager to advance and build a strong network.",
     features: [
-      "Bi-weekly 1-on-1 sessions",
-      "Personalized learning path",
-      "Portfolio development",
-      "Interview preparation",
-      "Priority email support",
-      "Industry networking",
-      "Industry networking",
+      "Personalized mentorship plans tailored to your career goals.",
+      "Interview preparation, mock sessions, and career coaching.",
+      "Access to our vibrant Slack Channels, connecting you to mentors, industry experts, and alumni.",
+      "Collaborative project management using Jira for real-world experience.",
+      "Mid-tier project portfolio development for showcasing your skills.",
+      "Free access to select professional webinars and events.",
+      "Networking opportunities through exclusive mentor-hosted Q&A sessions.",
     ],
     badge: "Most popular",
+    comingSoon: false,
   },
   {
     package: "Expert Track",
     duration: "12 Months",
-    description: "Lorem Ipsum",
+    description:
+      "Designed for experts and leaders aiming for industry mastery.",
     features: [
-      "Weekly 1-on-1 sessions",
-      "Custom project mentoring",
-      "Job placement assistance",
-      "Technical interview prep",
-      "24/7 chat support",
-      "Industry connections",
-      "Certification guidance",
+      "Advanced mentorship with real-world project experience.",
+      "Job placement assistance with industry connections.",
+      "Access to Discord Channels, Slack, and other exclusive communities for 24/7 collaboration and networking.",
+      "Priority mentorship and resources, with robust project portfolios tailored to top-tier roles.",
+      "Advanced certifications in technical fields, leadership, and more.",
+      "Exclusive project showcases to industry leaders and hiring partners.",
+      "Unlimited access to premium tools and learning materials like online courses, software discounts, and research articles.",
     ],
-    badge: "coming soon",
+    badge: "",
+    comingSoon: true,
   },
 ];
 
@@ -66,17 +78,20 @@ const Mentorships = () => {
   return (
     <>
       {/* BANNER */}
-      <section className="py-24">
-        <div className="wrapper md:mx-auto md:max-w-4xl xl:max-w-5xl">
-          <div className="text-center md:mx-auto md:max-w-xl">
+      <section className="relative py-24 lg:py-28">
+        <BannerIllustration />
+        <div className="wrapper relative z-10 md:mx-auto md:max-w-4xl xl:max-w-5xl">
+          <div className="text-center md:mx-auto md:max-w-3xl">
             <h4 className="text-sm text-secondary-200">Mentorships</h4>
             <h1 className="my-4 text-3xl font-medium leading-[1.3] text-white sm:text-5xl sm:leading-snug">
               Join our Program
             </h1>
             <p className="font-light leading-normal text-primary-200">
-              Our mentorship programs are 99% hands-on. You get to design and
-              build outstanding projects that can give you an edge in a hiring
-              process.
+              Our mentorship programs are 99% hands-on, designed to immerse you
+              in real-world projects and provide you with the skills to stand
+              out in the tech industry. Whether you&apos;re a student, early
+              professional, or seasoned expert, we&apos;ve got a tailored track
+              for your growth.
             </p>
 
             <div className="mt-12 flex items-center justify-center gap-2">
@@ -105,9 +120,9 @@ const Mentorships = () => {
         <div className="">
           <h3 className="mb-2 text-2xl text-white">Our Certifications</h3>
           <p className="text-base font-light leading-normal text-primary-200">
-            Our mentorship programs are 99% hands-on. You get to design and
-            build outstanding projects that can give you an edge in a hiring
-            process.
+            ISentry issues Professional Certifications upon successful program
+            completion. These certifications are designed to boost your
+            credibility and demonstrate your expertise to potential employers.
           </p>
         </div>
         <div className="mt-6 rounded-2xl border border-[#F4F4F414] p-6 lg:mt-0">
@@ -115,18 +130,27 @@ const Mentorships = () => {
             Professional Certifications
           </h3>
           <div className="">
-            <h4 className="mb-3 text-base font-light text-white">
-              Technical Certifications
+            <h4 className="mb-3 text-lg font-light text-white">
+              Certification Includes:
             </h4>
-            <ul className="list-disc space-y-2 pl-5 text-base font-light leading-normal text-white">
-              <li>Full-Stack Development (MERN Stack)</li>
-              <li>Cloud Computing (AWS/Azure)</li>
-              <li>Data Science & Machine Learning</li>
-              <li> DevOps & CI/CD</li>
+            <ul className="list-disc space-y-2 pl-5 text-base font-light leading-normal text-primary-100">
+              <li>
+                Technical Certifications in Web Development, AI/ML, and Data
+                Science.
+              </li>
+              <li>Product Management Certifications.</li>
+              <li>UX/UI Design Credentials.</li>
+              <li>
+                Recognized Training in Leadership and Agile Methodologies.
+              </li>
             </ul>
+
+            <p className="mt-4 font-medium text-white">
+              Join us to earn credentials that employers trust.
+            </p>
           </div>
 
-          <div className="mt-8">
+          {/* <div className="mt-8">
             <h4 className="mb-3 text-base font-light text-white">
               Industry-Recognized Credentials
             </h4>
@@ -137,7 +161,7 @@ const Mentorships = () => {
               <li>Cybersecurity Fundamentals</li>
               <li>UI/UX Design</li>
             </ul>
-          </div>
+          </div> */}
         </div>
       </section>
 
@@ -149,9 +173,10 @@ const Mentorships = () => {
             Transform Your Business with Tailored Tech Solutions
           </h3>
           <p className="font-light leading-normal text-primary-200">
-            At I-Sentry Technologies, we offer a comprehensive range of
-            technology services designed to help your business thrive in the
-            digital age.
+            We deliver innovative, scalable, and results-driven technology
+            solutions to propel your business forward in today&apos;s digital
+            landscape. Partner with us to unlock new opportunities and optimize
+            your operations with cutting-edge expertise.
           </p>
         </div>
 
@@ -185,19 +210,19 @@ const solutions: SolutionProp[] = [
     icon: <Zap />,
     title: "Custom Software Development",
     description:
-      "From concept and design to development and deployment, we ensure your custom solution integrates seamlessly into your existing systems while offering scalability and flexibility for future growth.",
+      "From ideation to deployment, we craft bespoke software solutions that seamlessly integrate with your existing systems. With a focus on scalability, security, and flexibility, we help your business grow and adapt to ever-changing needs.",
   },
   {
     icon: <Zap />,
     title: "Cloud Solutions",
     description:
-      "Whether you need cloud storage, computing power, or software as a service (SaaS), we tailor our cloud services to fit your business model, enabling you to operate more efficiently while reducing overhead costs.",
+      "Leverage the power of the cloud to enhance efficiency and reduce operational costs. Whether it's storage, computing power, or SaaS applications, our solutions are designed to align with your business objectives and maximize performance.",
   },
   {
     icon: <Zap />,
     title: "Tech Consultancy",
     description:
-      "From initial planning and roadmapping to implementation and scaling, we work with you to ensure your technology aligns with your business goals, giving you a competitive edge in today’s digital world.",
+      "Empower your business with strategic technology planning and execution. From initial consultation to implementation, we ensure that your digital transformation aligns perfectly with your business goals, giving you a competitive edge.",
   },
 ];
 
@@ -212,11 +237,20 @@ const SolutionCard = ({ item }: { item: SolutionProp }) => {
         <p className="font-inter text-sm font-light text-primary-100 md:text-base">
           {item?.description}
         </p>
+
+        <div className="mt-2">
+          <h4 className="font-medium text-white">Key Features</h4>
+          <ul className="list-disc pl-3.5 text-sm font-light leading-normal text-white">
+            <li className="text-primary-100">
+              Comprehensive digital roadmaps for growth.
+            </li>
+          </ul>
+        </div>
         <SmartButton
           showAnimatedIcon
           variant="dark"
           buttonText="Schedule a call"
-          className="px-6 py-3"
+          className="mt-4 object-cover object-center px-6 py-3"
           isLink
           url="https://calendly.com/isentrytechnologies"
         />
